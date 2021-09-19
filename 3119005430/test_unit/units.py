c006ea09fdf32fa80d98fb1786c9f03de58ae62b
3119005430 <1026762224@qq.com>
2021-09-19 21:19:01 +0800

    没完儿

--- 3119005430/test_unit/units.py
+++ 3119005430/test_unit/units.py
@@ -1,27 +1,27 @@
-import unittest
+import pytest
 from main.main import Main
 
-class Test(unittest.TestCase):
+class Test_unit(unittest.TestCase):
 
     # 测试orig.txt与orig_0.8_add.txt
     def test1(self):
-        Main('../textfile/orig.txt', '../textfile/orig_0.8_add.txt', '../result.txt')
+        Main('../main/texts/orig.txt', '../main/texts/orig_0.8_add.txt', '../result.txt')
 
     # 测试orig.txt与orig_0.8_del.txt
     def test2(self):
-        Main('../textfile/orig.txt', '../textfile/orig_0.8_del.txt', '../result.txt')
+        Main('../main/texts/orig.txt', '../main/texts/orig_0.8_del.txt', '../result.txt')
 
     # 测试orig.txt与orig_0.8_dis_1.txt
     def test3(self):
-        Main('../textfile/orig.txt', '../textfile/orig_0.8_dis_1.txt', '../result.txt')
+        Main('../main/texts/orig.txt', '../main/texts/orig_0.8_dis_1.txt', '../result.txt')
 
     # 测试orig.txt与orig_0.8_dis_10.txt
     def test4(self):
-        Main('../textfile/orig.txt', '../textfile/orig_0.8_dis_10.txt', '../result.txt')
+        Main('../main/texts/orig.txt', '../main/texts/orig_0.8_dis_10.txt', '../result.txt')
 
     # 测试orig.txt与orig_0.8_dis_15.txt
     def test5(self):
-        Main('../textfile/orig.txt', '../textfile/orig_0.8_dis_15.txt', '../result.txt')
+        Main('../main/texts/orig.txt', '../main/texts/orig_0.8_dis_15.txt', '../result.txt')
 
 
 if __name__ == '__main__':
